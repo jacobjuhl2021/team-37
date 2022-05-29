e728ba6669d0c4695a4310a5cfbbba2d2d5a8e5d
Nikolaihoej <nhje37914@edu.ucl.dk>
2022-05-29 10:12:45 +0200

    asd
    safas

--- js/menu.js
+++ js/menu.js
@@ -1,4 +1,4 @@
-/* menubar js */
+//javascript af Nikolai
 /* Acess - hvilke elementer skal aktiveres */
 const hamburger = document.querySelector(".hamburger");
 const navMenu = document.querySelector(".nav-menu");
